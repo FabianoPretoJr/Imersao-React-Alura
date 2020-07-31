d37b54e44bea5e88bf059c7b9ad4c1799ceb3f75
FabianoPretoJr <61958796+FabianoPretoJr@users.noreply.github.com>
2020-07-30 22:37:39 -0300

    adc link do back deployado

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -37,7 +37,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://fabianflix.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
